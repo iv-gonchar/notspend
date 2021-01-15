3d038c8e9de5f9cb9c1f5f6d76e4e9ab677b3324
iv.gonchar <gonchar.ivan@gmail.com>
2021-01-15 15:23:01 +0200

    Add CurrencyJpaService

--- src/main/java/com/notspend/repository/CurrencyRepository.java
+++ src/main/java/com/notspend/repository/CurrencyRepository.java
@@ -1,11 +1,22 @@
 package com.notspend.repository;
 
 import com.notspend.entity.Currency;
+import com.notspend.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CurrencyRepository extends Repository<Currency, String> {
 
-    Optional<Currency> findByNumber(Integer number);
+    List<Currency> getAll();
+
+    @Query("SELECT DISTINCT cur FROM currency AS cur WHERE EXISTS " +
+            "(SELECT acc FROM account AS acc WHERE acc.currency = cur AND acc.user = ?1)")
+    List<Currency> getAssignedToUser(User user);
+
+    Optional<Currency> getByCode(String code);
+
+    Optional<Currency> getByNumber(Integer number);
 }
---
+++ src/main/java/com/notspend/service/jpa/CurrencyJpaService.java
@@ -0,0 +1,44 @@
+package com.notspend.service.jpa;
+
+import com.notspend.entity.Currency;
+import com.notspend.repository.CurrencyRepository;
+import com.notspend.service.CurrencyService;
+import com.notspend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class CurrencyJpaService implements CurrencyService {
+
+    private final CurrencyRepository currencyRepository;
+
+    private final UserService userService;
+
+    @Override
+    public Currency getCurrencyByCode(String code) {
+        return currencyRepository.getByCode(code).orElseThrow(
+                () -> new NoSuchElementException("There is no currency with code " + code + " in repository")
+        );
+    }
+
+    @Override
+    public Currency getCurrencyByNumber(Integer number) {
+        return currencyRepository.getByNumber(number).orElseThrow(
+                () -> new NoSuchElementException("There is no currency with number " + number + " in repository")
+        );
+    }
+
+    @Override
+    public List<Currency> getAllCurrencies() {
+        return currencyRepository.getAll();
+    }
+
+    @Override
+    public List<Currency> getAllCurrenciesAssignedToUser() {
+        return currencyRepository.getAssignedToUser(userService.currentUser());
+    }
+}
