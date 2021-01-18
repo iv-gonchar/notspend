7e97ee4ee85e5c2250e92ffa28d291b99970a500
iv.gonchar <gonchar.ivan@gmail.com>
2021-01-18 19:56:43 +0200

    Add Transactional on CategoryJpaService.deleteCategoryById

--- src/main/java/com/notspend/service/jpa/CategoryJpaService.java
+++ src/main/java/com/notspend/service/jpa/CategoryJpaService.java
@@ -9,6 +9,7 @@ import com.notspend.util.SecurityUserHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -57,6 +58,7 @@ public class CategoryJpaService implements CategoryService {
     }
 
     @Override
+    @Transactional
     public void deleteCategoryById(int id) {
         categoryRepository.deleteByIdAndUser(id, userService.currentUser());
     }
